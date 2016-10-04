b3442cfae4ae154a6c93754a90ef6f66c7467a6b
Eddy Luo <edluo1@bu.edu>
2016-10-03 20:07:41 -0400

    Added base Android files

---
+++ tamid/src/main/java/org/globalappinitiative/tamid/SplashActivity.java
@@ -0,0 +1,23 @@
+package org.globalappinitiative.tamid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISP_LEN = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the MainActivity. */
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISP_LEN);
+    }
+}
