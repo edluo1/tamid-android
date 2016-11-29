a98daaad0078403e36822e29d151354f06c00229
Eddy Luo <edluo1@bu.edu>
2016-11-28 20:06:51 -0500

    Added some stuff to deal with Firebase posts

---
+++ tamid/src/main/java/org/globalappinitiative/tamid/Post.java
@@ -0,0 +1,53 @@
+package org.globalappinitiative.tamid;
+
+/**
+ * Created by Edward on 11/17/2016.
+ */
+
+public class Post {
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public String description;
+    //public String userName;
+    public String imageUrl;
+    public int likes;
+
+    public Post() {
+
+    }
+    public Post(String desc) {
+        description = desc;
+        imageUrl = "";
+        likes = 0;
+    }
+
+    public Post(String desc, String url) {
+        description = desc;
+        imageUrl = url;
+        likes = 0;
+    }
+    public Post(String description, String imageUrl, int likes) {
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.likes = likes;
+    }
+
+    public void like() {
+        // deal with integer overflow
+        if (likes+1 > likes) likes++;
+    }
+
+    public void unlike() {
+        if (likes > 0) likes--;
+    }
+}
--- tamid/src/main/java/org/globalappinitiative/tamid/PostCardFragment.java
+++ tamid/src/main/java/org/globalappinitiative/tamid/PostCardFragment.java
@@ -5,12 +5,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by edLuo1 on 11/7/2016.
  */
 
 public class PostCardFragment extends Fragment {
+
+    private Post p;
+
+    public PostCardFragment() {
+
+    }
+
+    public PostCardFragment(Post p) {
+        // empty for fragment
+        this.p = p;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -18,4 +31,13 @@ public class PostCardFragment extends Fragment {
         return inflater.inflate(R.layout.post_layout, container, false);
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        TextView tvDesc = (TextView) getView().findViewById(R.id.tvDescription);
+        TextView tvLikes = (TextView) getView().findViewById(R.id.tvLikesCount);
+
+        tvDesc.setText(p.description);
+        tvLikes.setText(p.likes);
+    }
 }
